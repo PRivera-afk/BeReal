4240a1f7f5396686660fa62e431f1fef79ebef01
Pablo Rivera <pabloslab@Pablos-MacBook-Pro.local>
2025-11-23 19:05:43 -0500

    Initial Commit

---
+++ BeReal/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  BeReal
+//
+//  Created by Pablo Rivera on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
